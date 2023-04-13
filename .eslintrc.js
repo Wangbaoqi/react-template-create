43b8bc3fb0998596b60cf6d4b2554c82575c7bf6
Wangbaoqi <wangbaoqi8839@gmail.com>
2023-04-13 19:19:11 +0800

    add husky

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  root: true,
+  extends: [
+    "react-app",
+    "react-app/jest"
+  ],
+  overrides: [
+    {
+      "files": ["**/*.ts?(x)"],
+      "rules": {
+        
+      }
+    }
+  ]
+}
